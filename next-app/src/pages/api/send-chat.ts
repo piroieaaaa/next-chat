676fdf7b562d22635959e926f13326ac9c5b465e
5013269 <Takizawa.Tomohiro@exc.epson.co.jp>
2023-06-01 10:11:24 +0900

    コメント追加

--- next-app/src/pages/api/send-chat.ts
+++ next-app/src/pages/api/send-chat.ts
@@ -8,9 +8,12 @@ const SendChatHandler = (req: NextApiRequest, res: NextApiResponseWithSocket ) =
         return res.status(405).end();
     }
 
+    // WebSocketサーバーの起動確認
     if (res.socket.server.io !== undefined) {
+        // イベント定義が欲しいので、一旦変数に入れる。
         const io: IOServer<ClientToServerEvents, ServerToClientEvents> = res.socket.server.io
 
+        // POSTの内容を全クライアントに送信
         console.log(`send-chat api request: ${JSON.stringify(req.body)}`);
         io.emit('chat', req.body.message);
     }
